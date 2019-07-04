7d484e402649c1f90afe61c5da625c6108698dc1
Swimming_Kim <tge1375@naver.com>
2019-07-04 19:18:15 +0900

    bofore refactorying

--- ghostKickTracking.js
+++ ghostKickTracking.js
@@ -1,15 +1,25 @@
-const fs = require('fs');
-const puppeteer = require("puppeteer");
-const schedule  = require('node-schedule');
-const moment = require('moment');
-
 const csvWriter = require('csv-write-stream');
-const csvFilename = 'test.csv';
+const schedule  = require('node-schedule');
+const puppeteer = require("puppeteer");
+const moment = require('moment');
+const fs = require('fs');
 
-let kickNow = null;
-let xingNow = null;
-let gogoNow = null;
-let timeNow = null;
+let prevDate = null;
+
+// let kickNow = null;
+// let xingNow = null;
+// let gogoNow = null;
+// let timeNow = null;
+
+function isDateSame(date){
+    if(prev_time === date){
+        return true;
+    }else{
+        console.log('Another Date!!');
+		prev_time = date;
+		return false;
+    }
+}
 
 function writeCsv( time, kick, xing, gogo){
 	if (!fs.existsSync(csvFilename)) {
@@ -37,6 +47,12 @@ function writeCsv( time, kick, xing, gogo){
 }
 
 async function ghostKickTracker(){
+    let fullTimeString = moment().format('YYYY-MM-DD-HH:mm:ss');
+    let dateNow = fullTimeString.substring(0, 10);
+    let timeNow = fullTimeString.substring(11, 16);
+
+    console.log(dateNow, timeNow);
+
 	const browser = await puppeteer.launch({
 		headless : true,	// 헤드리스모드의 사용여부를 묻는다.
 		devtools : true	// 개발자 모드의 사용여부를 묻는다.
@@ -51,23 +67,42 @@ async function ghostKickTracker(){
 		return data;
 	}
 	
-	kickNow = await getHTML('Kickgoing');
-	xingNow = await getHTML('Xingxing');
-	gogoNow = await getHTML('Gogossing');
-	timeNow = moment().format('YYYY-MM-DD-HH:mm:ss');
-	
-	// console.log("Kickgoing : ", kickNow);
-	// console.log("Xingxing : ", xingNow);
-	// console.log("Gogossing : ", gogoNow);
-	// console.log(timeNow);
-	await writeCsv( timeNow, kickNow, xingNow, gogoNow );
-	browser.close();
+	let kickNow = await getHTML('Kickgoing');
+	let xingNow = await getHTML('Xingxing');
+	let gogoNow = await getHTML('Gogossing');
+
+    let csvFilename = `${dateNow}.csv`;
+    
+    if(prevDate === dateNow){ // Same Date
+        writer = csvWriter({sendHeaders: false});
+        writer.pipe(fs.createWriteStream(csvFilename, {flags: 'a'}));
+        writer.write({
+            header1: timeNow,
+            header2: kickNow,
+            header3: xingNow,
+            header4: gogoNow
+        });
+        writer.end();
+    }else{ // Another Date
+		writer = csvWriter({sendHeaders: false});
+		writer.pipe(fs.createWriteStream(csvFilename));
+		writer.write({
+			header1: 'Moment',
+			header2: 'Kickgoing',
+			header3: 'XingXing',
+			header4: 'Gogossing'
+        });
+        writer.write({
+            header1: timeNow,
+            header2: kickNow,
+            header3: xingNow,
+            header4: gogoNow
+        })
+        writer.end();
+        prevDate = dateNow;
+    }
 };
 
-const job = schedule.scheduleJob(`*/10 * * * *`, () => {
+const job = schedule.scheduleJob(`*/10 * * * * *`, () => {
 	ghostKickTracker();
-	console.log("Kickgoing : ", kickNow);
-	console.log("Xingxing : ", xingNow);
-	console.log("Gogossing : ", gogoNow);
-	console.log(timeNow);
 });
