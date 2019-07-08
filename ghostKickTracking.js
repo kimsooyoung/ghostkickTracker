64712e21cdfc6c360f393d4452370a0d6a955d0c
Swimming_Kim <tge1375@naver.com>
2019-07-08 19:14:13 +0900

    Date-Bug-Fixed

--- ghostKickTracking.js
+++ ghostKickTracking.js
@@ -4,8 +4,6 @@ const puppeteer = require("puppeteer");
 const moment = require('moment');
 const fs = require('fs');
 
-let prevDate = null;
-
 async function ghostKickTracker(){
     let fullTimeString = moment().format('YYYY-MM-DD-HH:mm:ss');
     let dateNow = fullTimeString.substring(0, 10);
@@ -19,11 +17,15 @@ async function ghostKickTracker(){
 	});
 	const page = await browser.newPage();
 	await page.goto( "https://ghostkick.net/live", { waitUntil : "networkidle2" } );
-	await page.waitFor(500);
+	await page.waitFor(200);
 
 	async function getHTML( company ){
 		let htmlComp = await page.waitFor( `body > app-root > main > app-live > app-scooter-state > table > tbody > tr > td.dot${company}` );
-		let data = await page.evaluate( htmlComp => htmlComp.textContent, htmlComp);
+        let data = await page.evaluate( htmlComp => htmlComp.textContent, htmlComp);
+        if(data === 0){
+            htmlComp = await page.waitFor( `body > app-root > main > app-live > app-scooter-state > table > tbody > tr > td.dot${company}` );
+            data = await page.evaluate( htmlComp => htmlComp.textContent, htmlComp);
+        }
 		return data;
 	}
 	
@@ -32,9 +34,9 @@ async function ghostKickTracker(){
 	let gogoNow = await getHTML('Gogossing');
 
     let csvFilename = `${dateNow}.csv`;
-	writer = csvWriter({sendHeaders: false});
-    
-    if(prevDate === dateNow){ // Same Date
+    writer = csvWriter({sendHeaders: false});
+
+    if(fs.existsSync(csvFilename)){
         writer.pipe(fs.createWriteStream(csvFilename, {flags: 'a'}));
         writer.write({
             header1: timeNow,
@@ -43,9 +45,9 @@ async function ghostKickTracker(){
             header4: gogoNow
         });
         writer.end();
-    }else{ // Another Date
-		writer.pipe(fs.createWriteStream(csvFilename));
-		writer.write({
+    }else{
+        writer.pipe(fs.createWriteStream(csvFilename));
+        writer.write({
 			header1: 'Moment',
 			header2: 'Kickgoing',
 			header3: 'XingXing',
@@ -58,10 +60,36 @@ async function ghostKickTracker(){
             header4: gogoNow
         })
         writer.end();
-        prevDate = dateNow;
     }
+
+    // if(prevDate === dateNow){ // Same Date
+    //     writer.pipe(fs.createWriteStream(csvFilename, {flags: 'a'}));
+    //     writer.write({
+    //         header1: timeNow,
+    //         header2: kickNow,
+    //         header3: xingNow,
+    //         header4: gogoNow
+    //     });
+    //     writer.end();
+    // }else{ // Another Date
+	// 	writer.pipe(fs.createWriteStream(csvFilename));
+	// 	writer.write({
+	// 		header1: 'Moment',
+	// 		header2: 'Kickgoing',
+	// 		header3: 'XingXing',
+	// 		header4: 'Gogossing'
+    //     });
+    //     writer.write({
+    //         header1: timeNow,
+    //         header2: kickNow,
+    //         header3: xingNow,
+    //         header4: gogoNow
+    //     })
+    //     writer.end();
+    //     prevDate = dateNow;
+    // }
 };
 
-const job = schedule.scheduleJob(`*/10 * * * *`, () => {
+const job = schedule.scheduleJob(`*/10 * * * * *`, () => {
 	ghostKickTracker();
 });
